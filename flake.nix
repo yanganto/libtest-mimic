0047a9f1dbf5ca128028b8f729109ec690f049a5
Antonio Yang <yanganto@gmail.com>
2024-12-06 10:51:01 +0800

    update dependency

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        rust = pkgs.rust-bin.stable."1.75.0".default;
+        rust = pkgs.rust-bin.stable."1.83.0".default;
         dr = dependency-refresh.defaultPackage.${system};
 
         publishScript = pkgs.writeShellScriptBin "crate-publish" ''
